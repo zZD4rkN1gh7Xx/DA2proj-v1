774b1613d8f9a75fc0b045c7a96824747a17d109
up202108882 <up202108882@up.pt>
2024-05-18 14:27:26 +0100

    teste

--- main.cpp
+++ main.cpp
@@ -20,30 +20,32 @@ int main(void)
     //../files/Toy-Graphs/Toy-Graphs/tourism.csv
     //FileReaderToy::add_all_toy("D:\\FEUP 2ano\\2 semestre\\Desenho de algoritmos\\DAProj2_v2\\DA2proj-v1\\files\\Toy-Graphs\\Toy-Graphs\\shipping.csv", current_graph);
     FileReaderToy::add_all_places_coordinates("D:\\FEUP 2ano\\2 semestre\\Desenho de algoritmos\\DAProj2_v2\\DA2proj-v1\\files\\Extra_Fully_Connected_Graphs\\Extra_Fully_Connected_Graphs\\nodes.csv", current_graph);
-    FileReaderToy::add_all_connections_coordinates("D:\\FEUP 2ano\\2 semestre\\Desenho de algoritmos\\DAProj2_v2\\DA2proj-v1\\files\\Extra_Fully_Connected_Graphs\\Extra_Fully_Connected_Graphs\\edges_25.csv", current_graph);
+    FileReaderToy::add_all_connections_coordinates("D:\\FEUP 2ano\\2 semestre\\Desenho de algoritmos\\DAProj2_v2\\DA2proj-v1\\files\\Extra_Fully_Connected_Graphs\\Extra_Fully_Connected_Graphs\\edges_900.csv", current_graph,0);
     //FileReaderToy::add_all_toy("D:\\FEUP 2ano\\2 semestre\\Desenho de algoritmos\\DAProj2_v2\\DA2proj-v1\\files\\Toy-Graphs\\Toy-Graphs\\stadiums.csv", current_graph);
 
 
 
     //std::vector<int> a = tsp_triangular_aprox(current_graph);
+    std::vector<int> a = tsp_realworld(current_graph,0,3, 0.8);
 
     //
-    /*std::vector<int> a = tsp_simulated_annealing(current_graph, 1000, 0.995, 1000);
+    /*std::vector<int> a = tsp_simulated_annealing(current_graph, 1000, 0.995, 1000);*/
 
     for(auto b : a)
     {
-       std::cout << b << "->" ;
+        std::cout << b << "->" ;
     }
 
-    std::cout << calculate_total_distance(current_graph,a);*/
+    std::cout << calculate_total_distance(current_graph,a);
     //std::cout << std::endl;
     /*vector<Place> result = tspBacktrack(current_graph);
     for (auto a : result) {
         cout << a.get_id() << " - ";
     }*/
 
-    for(auto a : current_graph.getVertexSet()) {
+    /*for(auto a : current_graph.getVertexSet()) {
         std::cout << a->getInfo().get_id() << " " << std::setprecision(15)  << a->getInfo().get_longitude() << " " << std::setprecision(15) << a->getInfo().get_longitude()
                   << std::endl;
     }
+     */
 }
